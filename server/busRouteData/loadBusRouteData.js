fe1d25b1cb8125fbe282b4768ee242093c7bd2c3
tchamberlain <taylorannchamberlain@gmail.com>
2016-07-24 18:38:58 -0700

    Add working insert into routes, stops, and route_stop

--- server/busRouteData/loadBusRouteData.js
+++ server/busRouteData/loadBusRouteData.js
@@ -5,7 +5,6 @@ getBusRouteData();
 
 function getBusRouteData(){
   var url = 'https://data.cityofchicago.org/resource/5eg2-c264.json';
-  //var d = Q.defer();
   request(url, function (err, response, body) {
     if(err){
       console.log('There was an error:', err );
@@ -17,11 +16,6 @@ function getBusRouteData(){
 
 function storeBusRouteData( data ){
   for(var i = 0; i < data.length; i++){
-    // if there's only one route, its not in an arr
-    // so we put it into an arr
-    if( !Array.isArray(data[i].routes) ){
-      data[i].routes = [ data[i].routes ];
-    }  
     createStopEntry(data[i]);
   }
 }
@@ -29,29 +23,33 @@ function storeBusRouteData( data ){
 function createStopEntry(currStop){
   var alightings = currStop.alightings;
   var boardings = currStop.boardings;
-  var cross_street = currStop.cross_street;
-  var on_street = currStop.on_street;
+  var cross_street = currStop.cross_street.replace('"',"'").replace('"',"'");; // making quotes uniform
+  var on_street = currStop.on_street.replace('"',"'").replace('"',"'");; // making quotes uniform
   var lat = currStop.location.coordinates[1];
   var lng = currStop.location.coordinates[0];
   var stop_id = currStop.stop_id;
 
-  sqlRequest = "INSERT INTO 'stops' (alightings, boardings,cross_street,on_street,lat,lng,stop_id) " +
-            "VALUES('" + alightings + "', '" 
-                       + boardings + "', '" 
-                       + cross_street + "', '" 
-                       + on_street + "', '" 
-                       + lat + "', '" 
-                       + lng + "', '" 
+  sqlRequest = 'INSERT INTO "stops" (alightings, boardings,cross_street,on_street,lat,lng,stop_id) ' +
+            'VALUES("' + alightings + '", "' 
+                       + boardings + '", "' 
+                       + cross_street + '", "' 
+                       + on_street + '", "' 
+                       + lat + '", "' 
+                       + lng + '", "' 
                        + stop_id 
-                       + "')"
+                       + '")'
   db.run(sqlRequest, function(err) {
     if(err !== null) {
       // console.log('******',alightings, boardings, cross_street, on_street,lat, lng, stop_id);
      console.log(cross_street);
-      // console.log('ERROR',err);
+      console.log('ERROR',err);
     } else {
-      for(var i = 0; i < currStop.routes.length;  i++){
-        createRouteEntry(currStop.routes[i], stop_id);
+      if(currStop.routes){
+        // turn the list into an array
+        currStop.routes =  (currStop.routes).split(',');
+        for(var i = 0; i < currStop.routes.length;  i++){
+          createRouteEntry(currStop.routes[i], stop_id);
+        }
       }
     }
   });
